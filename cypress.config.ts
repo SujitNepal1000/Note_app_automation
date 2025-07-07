8c6d8e6e04233f0d5ca5692afeaf884f7b6a1fe6
Sujit NEPAL <sujitnepal27@gmail.com>
2025-07-07 14:43:31 +0545

    added project id for action

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'y3eqwy',
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     reportDir: 'reports/mochawesome',
